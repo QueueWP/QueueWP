8a40e0610f80da9b407e4d0c8d43d8010824652d
Matt Geri <mattgeri@gmail.com>
2017-02-08 11:24:14 +0200

    Meta Box Styles * Added styles for the meta box * Added a few new features to the meta box that needs to be built out

--- includes/admin/class-meta-box.php
+++ includes/admin/class-meta-box.php
@@ -23,12 +23,22 @@ use Social_Queue\Social_Queue;
 class Meta_Box {
 
 	/**
+	 * Meta Box Handle
+	 *
+	 * The handle used to register and enqueue scripts for the metabox.
+	 *
+	 * @since 0.1
+	 */
+	const META_BOX_STYLE_HANDLE = 'social_queue_meta_box_admin_css';
+
+	/**
 	 * Meta_Box constructor.
 	 *
 	 * @since 0.1
 	 */
 	public function __construct() {
 		add_action( 'add_meta_boxes', array( $this, 'create_meta_box' ), 1 );
+		add_action( 'admin_enqueue_scripts', array( $this, 'meta_box_styles' ) );
 	}
 
 	/**
@@ -58,5 +68,14 @@ class Meta_Box {
 	 */
 	public function render_meta_box() {
 		Social_Queue::get()->utility->template->load( 'admin/meta-box.php' );
+	}
+
+	public function meta_box_styles() {
+		wp_register_style( self::META_BOX_STYLE_HANDLE, Social_Queue::get()->plugin_url . 'assets/css/admin/meta-box.css', false, '0.1' );
+
+		/**
+		 * @todo: we should only enqueue this if we're on a admin page which shows the metabox.
+		 */
+		wp_enqueue_style( self::META_BOX_STYLE_HANDLE );
 	}
 }
--- templates/admin/meta-box.php
+++ templates/admin/meta-box.php
@@ -3,22 +3,35 @@
 		<div class="social-queue-networks">
 			<ul>
 				<li>
-					<input type="checkbox"><label for="">Twitter</label>
-					<input type="checkbox"><label for="">Facebook</label>
-					<input type="checkbox"><label for="">LinkedIn</label>
+					<input type="checkbox"><span class="dashicons dashicons-twitter"></span>
+				</li>
+                <li>
+                    <input type="checkbox"><span class="dashicons dashicons-facebook"></span>
 				</li>
 			</ul>
 		</div>
 
 		<div class="social-queue-description">
-			<textarea placeholder="Start typing your text for this social media post..."></textarea>
+			<textarea placeholder="<?php esc_attr_e( 'Start typing the message you would like to share...', 'social-queue' ); ?>"></textarea>
 		</div>
 
 		<div class="social-queue-preview">
-			Post preview
+			<h3><?php esc_html_e( 'Social Media Preview', 'social-queue' ); ?></h3>
 		</div>
 	</div>
 	<div class="social-queue-schedule">
-		<h3>Schedule</h3>
+		<h3><?php esc_html_e( 'Schedule Post', 'social-queue' ); ?></h3>
+
+        <ul>
+            <li>
+                <input name="social-queue-schedule" type="radio" checked><label><?php esc_html_e( "Don't schedule", 'social-queue' ); ?></label>
+            </li>
+            <li>
+                <input name="social-queue-schedule" type="radio"><label><?php esc_html_e( 'Automatically schedule for me', 'social-queue' ); ?></label>
+            </li>
+            <li>
+                <input name="social-queue-schedule" type="radio"><label><?php esc_html_e( 'Let me choose the schedule', 'social-queue' ); ?></label>
+            </li>
+        </ul>
 	</div>
 </div>
